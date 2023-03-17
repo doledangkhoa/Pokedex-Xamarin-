a4af5d7cf35e194c362a792717712eb2f4d65f78
doledangkhoa <mrnst21@gmail.com>
2023-03-18 00:49:20 +0700

    upload to github

---
+++ PokedexXF/PokedexXF/ViewModels/LoginViewModel.cs
@@ -0,0 +1,61 @@
+﻿using PokedexXF.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PokedexXF.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public LoginViewModel()
+        {
+
+        }
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+        public Command LoginCommand
+        {
+            get
+            {
+                return new Command(Login);
+            }
+        }
+
+        private void Login()
+        {
+            if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))
+                App.Current.MainPage.DisplayAlert("Error", "Please enter email and password", "OK");
+            else
+            {
+                if (Email == "admin@gmail.com" && Password == "12345")
+                {
+                    App.Current.MainPage.Navigation.PushAsync(new HomePage());
+                }
+                else
+                    App.Current.MainPage.DisplayAlert("Error", "Incorrect email or password", "OK");
+            }
+        }
+
+    }
+}
